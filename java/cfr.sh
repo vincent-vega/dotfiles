d2cfdbea03c50dcf34dbe9f4e1aa608844555188
Davide Barelli <>
2020-05-21 15:52:38 +0100

    arch checks

--- java/cfr.sh
+++ java/cfr.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-java -jar ${HOME}/dotfiles/java/cfr-0.149.jar "$@"
-#java -jar $(cygpath -w ${HOME}/dotfiles/java/cfr-0.149.jar) "$@"
+if [[ "$(uname -s)" == "CYGWIN"* ]]; then
+    jarpath=$(cygpath -w ${HOME}/dotfiles/java/cfr-0.149.jar)
+    classes=$(cygpath -w "$@")
+else
+    jarpath="${HOME}/dotfiles/java/cfr-0.149.jar"
+    classes="$@"
+fi
+if [ ! -f $jarpath ]; then
+    echo "jar lib $jarpath not found"
+    return 1
+fi
+java -jar $jarpath $classes
