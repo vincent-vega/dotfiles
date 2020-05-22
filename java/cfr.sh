8d824837dbd8059ad9007417c3dd3f21bc365c16
Davide Barelli <bar.davide@gmail.com>
2020-05-22 22:20:41 +0100

    dynamic path detection

--- java/cfr.sh
+++ java/cfr.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
+
+# get the script directory path
+source="${BASH_SOURCE[0]}"
+while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
+  dir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+  source="$(readlink "$source")"
+  [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+scriptdir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+
 if [[ "$(uname -s)" == "CYGWIN"* ]]; then
-    jarpath=$(cygpath -w ${HOME}/dotfiles/java/cfr-0.149.jar)
+    jarpath=$(cygpath -w "$scriptdir/cfr-0.149.jar")
     classes=$(cygpath -w "$@")
 else
-    jarpath="${HOME}/dotfiles/java/cfr-0.149.jar"
+    jarpath="$scriptdir/cfr-0.149.jar"
     classes="$@"
 fi
 if [ ! -f $jarpath ]; then
