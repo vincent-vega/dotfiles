3355aaa48fc6b7cc95b2c41cbd8e4186d80196a1
Davide Barelli <bar.davide@gmail.com>
2020-06-09 19:16:16 +0100

    warnings cleanup

--- java/cfr.sh
+++ java/cfr.sh
@@ -5,21 +5,21 @@ javadecompiler="cfr-0.149.jar"
 # get the script directory path
 source="${BASH_SOURCE[0]}"
 while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
-  dir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+  dir="$( cd -P "$( dirname "$source" )" > /dev/null 2>&1 && pwd )"
   source="$(readlink "$source")"
   [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-scriptdir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+scriptdir="$( cd -P "$( dirname "$source" )" > /dev/null 2>&1 && pwd )"
 
 if [[ "$(uname -s)" == "CYGWIN"* ]]; then
     jarpath=$(cygpath -w "$scriptdir/$javadecompiler")
     classes=$(cygpath -w "$@")
 else
     jarpath="$scriptdir/$javadecompiler"
-    classes="$@"
+    classes="$*"
 fi
-if [ ! -f $jarpath ]; then
-    echo "jar lib $jarpath not found"
+if [ ! -f "$jarpath" ]; then
+    echo "java decompiler $jarpath not found"
     return 1
 fi
-java -jar $jarpath $classes
+java -jar "$jarpath" "$classes"
