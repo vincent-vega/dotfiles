dcd1adcc9895daec59a692cfe8208161f20fbf59
vincent-vega <>
2021-10-14 13:51:45 +0100

    bash history util; current dir vim enhancement

---
+++ bin/clearhistorydups.py
@@ -0,0 +1,42 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import os
+import sys
+
+
+def err(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+if len(sys.argv) > 2 or len(sys.argv) == 2 and sys.argv[1] in ('-h', '--help'):
+    err(f'USAGE: {sys.argv[0].split("/")[-1]} [PATH]')
+    err(f'       "{os.environ["HOME"]}/.bash_history" will be used if the PATH argument is not present.')
+    err(f'       The file will be edited in-place, you better create a backup before running this script.')
+    sys.exit(1)
+
+hist_file = f'{os.environ["HOME"]}/.bash_history' if len(sys.argv) == 1 else sys.argv[1]
+
+resp = None
+while resp is None or resp not in 'yYnN':
+    resp = input(f'File "{hist_file}" will be edited, continue? (y/N) ').strip()
+
+if resp in 'nN':
+    err('Aborted!')
+    sys.exit(2)
+
+with open(hist_file, 'r+') as f:
+    history = [ line.strip() for line in f.read().splitlines() ][::-1]
+    seen = set()
+    result = []
+    for h in filter(lambda x: x != '' and x not in seen, history):
+        result.append(h)
+        seen.add(h)
+    if result and result != history:
+        f.seek(0)
+        print(*result[::-1], file=f, sep='\n')
+        f.truncate()
+        err(f'{len(history) - len(result)} lines have been purged ({(len(history) - len(result)) / len(history) * 100:.1f}%)')
+    else:
+        err('No change needed')
