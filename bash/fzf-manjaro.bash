9514ceb8f1c958e097c1751a6c927a4c515b7634
Davide Barelli <bar.davide@gmail.com>
2023-01-06 14:04:19 +0100

    warnings removed

--- bash/fzf-manjaro.bash
+++ bash/fzf-manjaro.bash
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash
 [ -f /usr/share/fzf/completion.bash ] && source /usr/share/fzf/completion.bash
 [ -f /usr/share/fzf/key-bindings.bash ] && source /usr/share/fzf/key-bindings.bash
