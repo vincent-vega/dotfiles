8355be04caa3fd95a6bd326b284f6445281ec13b
Davide Barelli <bar.davide@gmail.com>
2023-01-23 21:05:38 +0100

    minor fixes

--- bash/fzf-manjaro.bash
+++ bash/fzf-manjaro.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
-[ -f /usr/share/fzf/completion.bash ] && source /usr/share/fzf/completion.bash
-[ -f /usr/share/fzf/key-bindings.bash ] && source /usr/share/fzf/key-bindings.bash
+[ -s /usr/share/fzf/completion.bash ] && source /usr/share/fzf/completion.bash
+[ -s /usr/share/fzf/key-bindings.bash ] && source /usr/share/fzf/key-bindings.bash
