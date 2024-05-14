7e1b7f585e920f138d3896a99c4efda25f72d5b8
Davide Barelli <bar.davide@gmail.com>
2024-05-14 19:51:11 +0200

    fzf update

--- bash/fzf-manjaro.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck shell=bash
-[ -s /usr/share/fzf/completion.bash ] && source /usr/share/fzf/completion.bash
-[ -s /usr/share/fzf/key-bindings.bash ] && source /usr/share/fzf/key-bindings.bash
