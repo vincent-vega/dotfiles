bc0170a596de20c7e7ecc339da69fe8d59b00d2c
Davide Barelli <bar.davide@gmail.com>
2026-01-04 17:57:36 +0100

    fixes

--- bin/tmux_workspace.sh
+++ bin/tmux_workspace.sh
@@ -10,12 +10,13 @@ tmux send-keys ' ranger' Enter
 tmux new-window -n 'mnt'
 tmux send-keys ' cd ~/Downloads' Enter
 tmux send-keys ' htop' Enter
+tmux send-keys '#' Enter
 tmux split-window -v
 tmux send-keys ' cd ~/Downloads' Enter
 tmux send-keys ' btop' Enter
 tmux split-window -v
 tmux send-keys ' cd ~/Downloads' Enter
-tmux send-keys ' while true; do ping www.reddit.com; sleep 2; done' Enter
+tmux send-keys ' while true; do ping -i 3 www.reddit.com; sleep 2; done' Enter
 
 tmux select-layout even-vertical
 tmux select-pane -t w:1.1
